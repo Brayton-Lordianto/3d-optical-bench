cb0c70b6838db1b5e3a3b5172be5d6d2ec9e9405
Brayton-Lordianto <braylord30@gmail.com>
2023-12-13 23:05:51 -0500

    some cylinder exists

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -11,8 +11,79 @@ uniform vec3 uColor;
 varying vec3 vNor;
 varying float fApplyTransform;
 
+vec3 bgColor = vec3(0.23, 0.6, 0.12);
+
+
+// Rotation matrix around the X axis.
+mat3 rotateX(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(vec3(1, 0, 0), vec3(0, c, -s), vec3(0, s, c));
+}
+
+// Rotation matrix around the Y axis.
+mat3 rotateY(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(vec3(c, 0, s), vec3(0, 1, 0), vec3(-s, 0, c));
+}
+
+
+float rayCylinder(vec3 rayOrigin, vec3 rayDir) {
+    vec3 adjustedRayOrigin = rayOrigin - vec3(1.5,1.5,0.);
+    float a = pow(rayDir.x, 2.0) + pow(rayDir.y, 2.0);
+    float b = 2.0 * (adjustedRayOrigin.x * rayDir.x + adjustedRayOrigin.y * rayDir.y);
+    float c = pow(adjustedRayOrigin.x, 2.0) + pow(adjustedRayOrigin.y, 2.0) - 1.;
+
+    float discriminant = pow(b, 2.0) - 4.0 * a * c;
+    float t1 = (-b - sqrt(discriminant)) / (2.0 * a);
+    float t2 = (-b + sqrt(discriminant)) / (2.0 * a);
+
+    // test both z values of each t
+    if (t1 < 0.0) {
+        return -1.0;
+    }
+    float z1 = adjustedRayOrigin.z + t1 * rayDir.z;
+    float z2 = adjustedRayOrigin.z + t2 * rayDir.z;
+    return z1; 
+
+    // if (discriminant < 0.0) {
+    //     return -1.0;
+    // }
+
+}
+
+float raySphere(vec3 rayOrigin, vec3 rayDir) {
+    vec3 center = vec3(0.,0.,-1.);
+    float a = dot(rayDir, rayDir);
+    float b = 2.0 * dot(rayOrigin - center, rayDir);
+    float c = dot(rayOrigin - center, rayOrigin - center) - 1.0;
+
+    float discriminant = pow(b, 2.0) - 4.0 * a * c;
+    float t = (-b - sqrt(discriminant)) / (2.0 * a);
+    return t;
+}
 
 void main(void) {
+    // initialize camera ray and color 
     vec3 color = vec3(0.0, 0.0, 0.0);
+    color = bgColor;
+    vec3 cameraOrigin = uCamera;
+    vec3 cameraDir = normalize(vec3(vPos.xy, -uFL));
+    cameraDir *= rotateY(-uCameraDirection.x);
+    cameraDir *= rotateX(-uCameraDirection.y);
+
+    // ray trace to a single cylinder
+    // float t = raySphere(cameraOrigin, cameraDir);
+    float t = rayCylinder(cameraOrigin, cameraDir);
+    float z = rayCylinder(cameraOrigin, cameraDir);
+    if (z > 0.0 && z < 3.) {
+        color = vec3(1.0, 0.0, 0.0);
+    } else if (z != -1.0) {
+        // color = vec3(0.,0.,1.);
+    }
+
+
+
     gl_FragColor = vec4(color, 1.0);
 }
